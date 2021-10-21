f25c0c06ba2ed253fa218736285ec6512476bcaa
Gabriel Souza <gsouza590@gmail.com>
2021-10-21 11:42:41 -0300

    camada de servico e registro componente

--- src/main/java/com/projetweb/course/repositories/UserRepository.java
+++ src/main/java/com/projetweb/course/repositories/UserRepository.java
@@ -1,7 +1,5 @@
 package com.projetweb.course.repositories;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.projetweb.course.entities.User;
---
+++ src/main/java/com/projetweb/course/services/UserService.java
@@ -0,0 +1,26 @@
+package com.projetweb.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.projetweb.course.entities.User;
+import com.projetweb.course.repositories.UserRepository;
+
+@Component // registra a classe como componente do Spring
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
